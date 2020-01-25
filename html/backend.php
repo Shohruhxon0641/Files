0561cb1bb25a3efb06d18414aba541f08366ab5c
Shohruhxon <meyliboyav@yandex.com>
2020-01-25 11:03:05 +0500

    Second

--- html/backend.php
+++ html/backend.php
@@ -1,3 +1,29 @@
-<?php
-    var_dump($_GET);
+ <?php
+    //var_dump($_GET);
     //var_dump($_POST);
+    $ism = $_POST['ism'];
+    $fam = $_POST['familiyasi'];
+    $sharif = $_POST['sharifi'];
+    $text = $_POST['matn'];
+    // php7 dan eski versiyalar uchun
+    if(isset($_POST['abs'])){
+        $abs = $_POST['abs'];
+    }
+    else { 
+        $abs = false;
+    }
+    //php7 dan yuqori versiyalar uchun
+    $abs = $_POST['abs'] ?? false;
+    $abc = $_POST['abc'] ?? false;
+    
+    var_dump($_POST); 
+    
+    $kurs = 
+
+
+
+
+
+
+
+
--- html/form.php
+++ html/form.php
@@ -3,9 +3,43 @@
         <title>FORM</title>
     </head>
     <body>
-        <form action="backend.php" method="GET">
-            <input type="text" value="site_title">
+        <form action="backend.php" method="POST">
+            <div>
+                <label for="">
+                    Ismi
+                    <input type="text" name="ism">
+                </label>
+            </div>
+           <div>
+                <label for="">
+                    Familiyasi
+                    <input type="text" name="familiyasi">
+                </label>
+            </div>
+            <div>
+                <label for="">
+                    Sharifi
+                    <input type="text" name="sharifi">
+                </label>
+            </div>
+            <div>
+                <label for="">
+                    Matn
+                    <textarea name="matn" id="" cols="30" rows="10"></textarea>
+                </label>
+            </div>
+            <div>
+                <label for="">
+                    Test uchun misollar
+                    <input type="radio" name="abs" value="a"> A) Javob 1
+                    <input type="radio" name ="abs" value="b"> B)Javob 2
+                    <input type="radio" name="abs" value="c"> C) Javob 3
+                </label>
+            </div>
+            <div>
+                <input type="checkbox" name="abc" value="85"> Maktabni bitirganmisiz?
+            </div>
             <button type="sumbit">Yuboriw</button>
         </form>
     </body>
-</html>
+</html> 
